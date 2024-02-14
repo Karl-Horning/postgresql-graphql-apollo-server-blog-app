4d0d18d0bb1affe2f8a7e8a56f306ff614dff66c
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-14 23:09:07 +0000

    Added schema and resolver to get all users

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,6 +1,39 @@
+/**
+ * Resolvers for GraphQL queries.
+ * @module resolvers
+ */
+
+const { User } = require("../models");
+
+/**
+ * GraphQL resolvers.
+ * @type {Object}
+ */
 const resolvers = {
     Query: {
+        /**
+         * Resolver function for the "hello" query.
+         * @returns {string} The string "World!"
+         */
         hello: () => "World!",
+
+        /**
+         * Resolver function for the "users" query.
+         * Retrieves all users from the database.
+         * @returns {Array} An array of user objects.
+         * @throws {Error} If there is an error fetching users.
+         */
+        users: async () => {
+            try {
+                // Retrieve all users from the database
+                const users = await User.findAll();
+                // Convert Sequelize model instances to plain JavaScript objects
+                return users.map((user) => user.get({ plain: true }));
+            } catch (error) {
+                console.error("Error fetching users:", error);
+                throw error;
+            }
+        },
     },
 };
 
--- src/schemas/queries.js
+++ src/schemas/queries.js
@@ -1,11 +1,60 @@
+/**
+ * GraphQL schema definition using Apollo Server's gql tag.
+ * @module typeDefs
+ */
+
 const { gql } = require("apollo-server");
 
+/**
+ * GraphQL schema definition.
+ * @type {string}
+ */
 const typeDefs = gql`
+    """
+    Represents a user in the system.
+    """
+    type User {
+        """
+        Unique identifier for the user.
+        """
+        userId: ID!
+
+        """
+        The username of the user.
+        """
+        username: String!
+
+        """
+        The email address of the user.
+        """
+        email: String!
+
+        """
+        The hashed password of the user.
+        """
+        passwordHash: String!
+
+        """
+        The date and time when the user was created.
+        """
+        createdAt: String!
+
+        """
+        The date and time when the user was last updated.
+        """
+        updatedAt: String!
+    }
+
     type Query {
         """
-        Test query
+        A test query to check if the server is running.
         """
         hello: String!
+
+        """
+        Get all users in the system.
+        """
+        users: [User!]!
     }
 `;
 
