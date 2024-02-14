7755052f96b3fe7b7f83522493b0a0203327feda
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-14 23:33:01 +0000

    Added JSDoc and comments

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,8 +1,19 @@
+/**
+ * Creates an executable GraphQL schema.
+ * @module schemas
+ */
+
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 
+// Import GraphQL schema definitions for queries
 const queries = require("./queries.schema");
+// Import resolvers to resolve queries
 const resolvers = require("../resolvers");
 
+/**
+ * Creates an executable GraphQL schema using provided type definitions and resolvers.
+ * @returns {Object} Executable GraphQL schema.
+ */
 const schema = makeExecutableSchema({
     typeDefs: queries,
     resolvers,
